9804c00f37e0a5accf3d3969a2101b5280f2aaf0
Uroš Šavelj <uros.savelj@tretton37.com>
2022-06-02 09:43:34 +0200

    upgrade to .net6

---
+++ FursTax/Http/SoapHttpService.cs
@@ -0,0 +1,48 @@
+﻿using MadWare.Furs.Requests;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MadWare.Furs.Http
+{
+    public class SoapHttpService : IHttpService
+    {
+        private X509Certificate2 cert;
+        private int timeout;
+
+        public SoapHttpService(X509Certificate2 cert, int timeoutSeconds = 3)
+        {
+            this.cert = cert;
+            this.timeout = timeoutSeconds;
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.ServerCertificateValidationCallback += new RemoteCertificateValidationCallback(this.RemoteCertificateValidationCallback);
+        }
+
+        protected virtual bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        public async Task<string> SendRequest(string url, string payload, BaseRequestBody b)
+        {
+            var handler = new HttpClientHandler();
+            handler.ClientCertificates.Add(this.cert);
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                client.DefaultRequestHeaders.Add("SOAPAction", b.GetSOAPAction());
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("text/xml"));
+                client.Timeout = TimeSpan.FromSeconds(this.timeout);
+
+                var cnt = new StringContent(payload, Encoding.UTF8, "text/xml");
+
+                HttpResponseMessage resp = await client.PostAsync(url, cnt).ConfigureAwait(false);
+
+                string content = await resp.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return content;
+            }
+        }
+    }
+}
---
+++ FursTax/ZOI/CertZOIProvider.cs
@@ -0,0 +1,77 @@
+﻿using MadWare.Furs.Models.Invoice;
+using MadWare.Furs.Requests;
+using System.Globalization;
+using System.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MadWare.Furs.ZOI
+{
+    public class CertZOIProvider : IZOIProvider
+    {
+        private X509Certificate2 cert;
+
+        public CertZOIProvider(X509Certificate2 cert)
+        {
+            this.cert = cert;
+        }
+
+        private string GetMD5Hash(byte[] input)
+        {
+            MD5 md5Hash = MD5.Create();
+
+            byte[] data = md5Hash.ComputeHash(input);
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++) { sBuilder.Append(data[i].ToString("x2")); }
+            return sBuilder.ToString();
+        }
+
+        public void CalculateZOI(BaseRequestBody b)
+        {
+            var inv = b as InvoiceRequestBody;
+            Invoice i = inv.InvoiceRequest.Invoice;
+
+            string data = string.Concat(i.TaxNumber,
+                                         i.IssueDateTime.Value.ToString("dd.MM.yyyy HH:mm:ss"),
+                                         i.InvoiceIdentifier.InvoiceNumber,
+                                         i.InvoiceIdentifier.BusinessPremiseID,
+                                         i.InvoiceIdentifier.ElectronicDeviceID,
+                                         i.InvoiceAmount.Value.ToString("N2", CultureInfo.InvariantCulture));
+
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+
+            var rsa = this.cert.GetRSAPrivateKey();
+
+            //RSACryptoServiceProvider rsaCsp = rsa as RSACryptoServiceProvider;
+
+            //CspParameters cspParameters = new CspParameters();
+            //cspParameters.KeyContainerName = rsaCsp.CspKeyContainerInfo.KeyContainerName;
+            //cspParameters.KeyNumber = rsaCsp.CspKeyContainerInfo.KeyNumber == KeyNumber.Exchange ? 1 : 2;
+
+            //RSACryptoServiceProvider rsaAesCSP = new RSACryptoServiceProvider(cspParameters);
+            //byte[] signature = rsaAesCSP.SignData(bytes, CryptoConfig.MapNameToOID("SHA256"));
+            byte[] signature = rsa.SignData(bytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+
+            string zoi = GetMD5Hash(signature);
+
+            i.ProtectedID = zoi;
+        }
+
+        public bool MustCalculateZOI(BaseRequestBody b)
+        {
+            if (b == null)
+                return false;
+
+            if (!(b is InvoiceRequestBody))
+                return false;
+
+            var invReqBody = b as InvoiceRequestBody;
+            if (invReqBody.InvoiceRequest == null || invReqBody.InvoiceRequest.Invoice == null)
+                return false;
+
+            return true;
+        }
+    }
+}
